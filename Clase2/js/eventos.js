80b54e2529fdb0160d775e67ccc813558c2c4239
deadwingg <ivgomez505@gmail.com>
2019-12-23 12:53:54 -0300

    Intro a callbacks

---
+++ Clase2/js/eventos.js
@@ -0,0 +1,16 @@
+console.log("connected");
+
+const uno = () => {
+    console.log('Soy la funcion 1')
+}
+const dos = () => {
+    console.log('Soy la funcion 2')
+}
+
+function prueba(item, callback) {
+    console.log(item);
+    callback();
+}
+
+prueba(1,dos);
+
