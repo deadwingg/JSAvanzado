81c4a97127274e164fbae2e87e6229d140827eb2
deadwingg <ivgomez505@gmail.com>
2019-12-30 13:02:09 -0300

    intro to regex

--- Clase2/js/eventos.js
+++ Clase2/js/eventos.js
@@ -1,95 +1,95 @@
-if (false){
-console.log("connected");
+if (false) {
+    console.log("connected");
 
-const uno = (e) => {
-    console.log('Soy la funcion 1');
-    console.log(e);
-};
-const dos = () => {
-    console.log('Soy la funcion 2')
-};
+    const uno = (e) => {
+        console.log('Soy la funcion 1');
+        console.log(e);
+    };
+    const dos = () => {
+        console.log('Soy la funcion 2')
+    };
 
-function prueba(item, callback) {
-    console.log(item);
-    callback();
-}
+    function prueba(item, callback) {
+        console.log(item);
+        callback();
+    }
 
-var button = document.querySelector('#btn');
+    var button = document.querySelector('#btn');
 
 //Via propiedad
-/*
-button.onclick = () => {
-    prueba("Propiedad", uno);
-}
- */
+    /*
+    button.onclick = () => {
+        prueba("Propiedad", uno);
+    }
+     */
 //Via event lister
-/*
-button.addEventListener("click",
-    () => prueba("Listener", dos));
+    /*
+    button.addEventListener("click",
+        () => prueba("Listener", dos));
 
- */
+     */
 
-button.addEventListener("click", uno);
-button.addEventListener("click", dos);
-button.addEventListener("click", () => console.log("Via Arrow"));
-button.addEventListener("click",
-    function(e){
-        console.log("Via funcion");
-        console.log(e);
-});
+    button.addEventListener("click", uno);
+    button.addEventListener("click", dos);
+    button.addEventListener("click", () => console.log("Via Arrow"));
+    button.addEventListener("click",
+        function (e) {
+            console.log("Via funcion");
+            console.log(e);
+        });
 //Se agregan todos, no como en el parametro que al cambiarlo se sobreescribe el valor anterior
 
 //Estructura de callbacks
 // En sintaxis tradicional
 
-function restar(a, b, cb){
-    if (cb){
-        cb(a, b, a-b);
+    function restar(a, b, cb) {
+        if (cb) {
+            cb(a, b, a - b);
+        }
     }
-}
 
-restar(5, 6 , function(a, b, res){
-    console.log(`El resultado de ${a} - ${b} es ${res}`);
-});
-
+    restar(5, 6, function (a, b, res) {
+        console.log(`El resultado de ${a} - ${b} es ${res}`);
+    });
 
 
 //Como arrow function
-const sumar = (a, b, cb) => {
-    if (cb){
-        cb(a, b, a+b);
-    }
-};
+    const sumar = (a, b, cb) => {
+        if (cb) {
+            cb(a, b, a + b);
+        }
+    };
 
-sumar(5, 6 ,
-    (a, b, res) => console.log(`El resultado de ${a} + ${b} es ${res}`));
+    sumar(5, 6,
+        (a, b, res) => console.log(`El resultado de ${a} + ${b} es ${res}`));
 
+}
+    let outer = document.querySelector("#outer");
+    let middle = document.querySelector("#middle");
+    let inner = document.querySelector("#inner");
 
-let outer= document.querySelector("#outer");
-let middle= document.querySelector("#middle");
-let inner= document.querySelector("#inner");
-
-outer.addEventListener("click", () => {
-    console.log("click 3")
-}, true);
-middle.addEventListener("click", () => {
-    console.log("click 2")
-}, true);
-inner.addEventListener("click", () => {
-    console.log("click 1")
-}, true);
+    outer.addEventListener("click", () => {
+        console.log("click 3")
+    }, !true);
+    middle.addEventListener("click", (e) => {
+        console.log("click 2");
+        e.stopPropagation();
+    }, !true);
+    inner.addEventListener("click", () => {
+        console.log("click 1")
+    }, !true);
 
 // la propagacion por defecto
 // es del elemento mas cercano al mas lejano, esto se llama "bubbling"
 // si quiero propagar al reves se llamaria "capturing"
 // con un tecer parametro en addEventListener puedo cambiar esta direccion
 
-}
+
 let btnCreado = false;
 let estatico = document.querySelector("#estatico");
 estatico.addEventListener('click', () => {
     console.log("Soy estático");
-    if (!btnCreado){
+    if (!btnCreado) {
         let dinamico = document.createElement('button');
         dinamico.innerText = "DINAMICO";
         dinamico.id = 'dinamico';
@@ -107,13 +107,39 @@ estatico.addEventListener('click', () => {
 document.addEventListener('click', (e) => {
     let id = e.target.id;
     // console.log('click sobre document', id);
-    if (id === 'dinamico'){
+    if (id === 'dinamico') {
         console.log('Soy dinamico');
     }
-    if (id === 'remove'){
+    if (id === 'remove') {
         document.body.removeChild(document.querySelector('#dinamico'));
         document.body.removeChild(document.querySelector('#remove'));
         btnCreado = false;
     }
-
 });
+
+//agregando un listener a un link
+document.querySelector('#link').addEventListener('click',
+    (e) => {
+        e.preventDefault(); // hace que no se ejecute el evento por defecto
+        console.log('Click en link')
+    });
+// Evento que se dispara cuando  cambia el tamaño interno del navegador
+window.addEventListener('resize', () => {
+    console.log('Cambio el tamaño del navegador');
+    document.querySelector('#parrafo').innerText =
+        `El tamaño interno del navegador es ${window.innerWidth} x ${window.innerHeight} px
+        El tamaño externo del navegador es ${window.outerWidth} x ${window.outerHeight} px`
+});
+
+//Custom events
+let ev = new Event('look',
+    {'bubbles': true, 'cancelable': false});
+
+document.addEventListener('look', () => {
+    console.log('Evento look');
+});
+
+setInterval(() => {
+    console.log('Intervalo de 1s');
+    document.dispatchEvent(ev);
+}, 1000);
---
+++ Clase3/js/forms.js
@@ -0,0 +1,54 @@
+let btn = document.querySelector('#btn');
+let input = document.querySelector('input');
+/*btn.addEventListener('click', (e)=> {
+    e.preventDefault();
+    //checkvalidity retorna true siempre, a menos que haya required y
+    //la entrada este vacía
+    console.log('click',input.checkValidity());
+
+});*/
+
+/*input.addEventListener('input', ()=> {
+   let dato = input.value; //trim corta espacios adelante y atras
+   let longitud = dato.length;
+   if(longitud > 3){
+       let datoTrim = dato.trim();
+       input.setCustomValidity('');
+   } else {
+       console.log('Este campo debe tener mas de 3 caracteres');
+   }
+   /!* else {
+       input.setCustomValidity('Este campo debe tener mas de 3 caracteres');
+   }*!/
+});*/
+let form = document.querySelector('#formulario');
+form.addEventListener('submit', (e)=> {
+    e.preventDefault();
+    let dato = input.value;
+    let longitud = dato.length;
+
+    //regexp
+    let miRegExp = /^[0-9]+[a-z]+$/;
+    if (miRegExp.test(dato)){
+        console.log('submit', dato);
+    } else {
+        console.log('Error en la entrada');
+    }
+    /*if(longitud > 3){
+        let datoTrim = dato.trim();
+        if(datoTrim.indexOf(' ') <= 0){
+            //con URI nos aseguramos de que no se pueda inyectar codigo
+            let datoSeguro = encodeURIComponent(datoTrim);
+            console.log('submit', datoTrim);
+        } else {
+            console.log('No se permiten espacios intermedios en este campo');
+        }
+        // input.setCustomValidity('');
+    } else {
+        console.log('Este campo debe tener mas de 3 caracteres');
+    }*/
+    /* else {
+        input.setCustomValidity('Este campo debe tener mas de 3 caracteres');
+    }*/
+
+});
