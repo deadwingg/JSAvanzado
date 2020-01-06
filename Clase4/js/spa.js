5b9868a9209288ad74aba6ed47820f53202ac41a
deadwingg <ivgomez505@gmail.com>
2020-01-06 12:59:48 -0300

    SPA skeleton done

--- Clase4/js/spa.js
+++ Clase4/js/spa.js
@@ -7,3 +7,22 @@ function ajax(url, metodo) {
     return xhr;
 }
 
+let main = document.querySelector('#main');
+console.log(main);
+let links = document.querySelectorAll('a');
+
+links.forEach(link => {
+    link.addEventListener('click', e => {
+        e.preventDefault();
+        let id = link.id;
+        console.log(id);
+        let archivo = id + '.html';
+        let xhr = ajax(archivo);
+        xhr.addEventListener('load', () => {
+            console.log(xhr.status);
+            if (xhr.status == 200) {
+                main.innerHTML = xhr.response;
+            }
+        })
+    })
+})
