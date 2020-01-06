b2f3bb1cc070dab74904d9595b34a01f989cca44
deadwingg <ivgomez505@gmail.com>
2020-01-06 09:57:41 -0300

    Continuing regex

---
+++ Clase3/js/regexp.js
@@ -0,0 +1,22 @@
+/*
+podemos usar regex de manera literal
+ */
+let literalRegExp = /123/;
+
+//O con un constructor
+let constructorRegExp = new ;
+
+
+/*
+tipos de caracteres:
+posicion : ^$
+literales : 123azAZ
+especiales: .|[]()
+cantidad: ?+*{}
+ */
+
+/*
+Especiales:
+. simboliza cualquier caracter
+
+ */
